9305d47b484524d363c2c4522eac6d80cd08d3b7
Rallie21m <rallie21m@yahoo.com>
2022-02-03 13:13:33 +0200

    Final version

--- src/main/java/qa/automation/Homework1.java
+++ src/main/java/qa/automation/Homework1.java
@@ -1,7 +1,19 @@
 package qa.automation;
 
+import java.util.Scanner;
+
 public class Homework1 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Plese enter your name:");
+
+        String name = scanner.nextLine();
+
+        System.out.println("Please enter your favourite hobby:");
+
+        String hobby = scanner.nextLine();
+
+        System.out.println("Your name is " + name + " and your favourite hobby is " + hobby);
     }
 }
